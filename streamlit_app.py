345414fa8e13ca6a9053cee7a44a7d353e4cb762
eeshmmajithiaee <eeshmajithia@icloud.com>
2024-08-04 14:12:57 -0400

    hi 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -557,7 +557,7 @@ def generatecontent(recordingmode = False):
 
         # The 'with' statement is used here to manage the spinner context
         with st.spinner("Initializing AI Model..."):
-            genai.configure(api_key=st.secrets[api_key])
+            genai.configure(api_key=st.secrets["api_key"])
 
             model = genai.GenerativeModel('gemini-1.5-flash')
 
@@ -760,7 +760,7 @@ def generatecontent(recordingmode = False):
 
         # The 'with' statement is used here to manage the spinner context
         with st.spinner("Initializing AI Video Model..."):
-            genai.configure(api_key='AIzaSyCZObffhLbps5I-NhjgDF5seb-eeZQ8bP8')
+            genai.configure(api_key=st.secrets["api_key"])
 
             model = genai.GenerativeModel('gemini-1.5-flash')
 
